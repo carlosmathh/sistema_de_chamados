41dc38c98ee13663c42822f559264296f6efbda0
Carlos Matheus <carlosmatheus23.com@gmail.com>
2026-02-18 15:18:18 -0300

    fix(cors): allow vercel origin

--- src/api/main.py
+++ src/api/main.py
@@ -11,18 +11,16 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://sistema-de-chamados-d8tf.vercel.app/",
+    "https://sistema-de-chamados-d8tf.vercel.app",
 ]
-
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(auth_router)
 app.include_router(search_router)
