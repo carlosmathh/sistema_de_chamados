f80eda20a7d70582c856f131771efcd5b6fb7367
Carlos Matheus <carlosmatheus23.com@gmail.com>
2026-02-18 13:38:42 -0300

    fix: add main.py entrypoint for deploy

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-# from src.app import App
-# funcs = App()
-
-
-# classes
-from src.auth.auth_flow import AuthFlow
-
-if __name__ == "__main__":
-    authflow = AuthFlow()
-    authflow.run()
+from src.api.main import app
