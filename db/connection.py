ac2cd7085582476eeddab942345972e48dc43d3f
Carlos Matheus <carlosmatheus23.com@gmail.com>
2026-04-26 14:43:19 -0300

    fix: test connection data base

--- db/connection.py
+++ db/connection.py
@@ -14,6 +14,7 @@ def get_connection() -> Connection:
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),  # type: ignore
         database=os.getenv("DB_NAME"),
+        connect_timeout=10,
         cursorclass=pymysql.cursors.DictCursor,
         autocommit=False,
     )
